7828be8fc46f6de9cc7e72e736371882c0e98bf3
Marcel Veselka <marcel.veselka@tesena.com>
2024-05-24 06:56:15 +0200

    chore: Improve visual testing validation in Wopee example

--- cypress/e2e/4-visual-test-exampls.cy.js
+++ cypress/e2e/4-visual-test-exampls.cy.js
@@ -7,33 +7,47 @@ before(() => {
   cy.wopeeStartSuite("Wopee visual testing example");
 });
 
-
 describe("Wopee visual testing benefits", () => {
-  it.skip("Long list of asserts", () => {
+  it("Long list of asserts", () => {
+    Cypress.on("uncaught:exception", (err, runnable) => {
+      return false;
+    });
+
     cy.visit(baseUrl);
     cy.get("button").contains("Odeslat registraci").click();
 
+    const errorText = "Toto pole je povinné";
     // Validate mandatory fields
-    cy.get("#taxId").should("have.class", "input-validation-error");
-    cy.get("#vatId").should("have.class", "input-validation-error");
-    cy.get("#companyName").should("have.class", "input-validation-error");
-    cy.get("#street").should("have.class", "input-validation-error");
-    cy.get("#zip").should("have.class", "input-validation-error");
-    cy.get("#city").should("have.class", "input-validation-error");
-    cy.get("#firstName").should("have.class", "input-validation-error");
-    cy.get("#lastName").should("have.class", "input-validation-error");
-    cy.get("#phone").should("have.class", "input-validation-error");
-    cy.get("#email").should("have.class", "input-validation-error");
+    cy.get("#taxId + div + span").should("contain.text", errorText);
+    cy.get("#vatId + div + span").should("contain.text", errorText);
+    cy.get("#companyName + div + span").should("contain.text", errorText);
+    cy.get("#street + div + span").should("contain.text", errorText);
+    cy.get("#zip + div + span").should("contain.text", errorText);
+    cy.get("#City + div + span").should("contain.text", errorText);
+    cy.get("#firstName + div + span").should("contain.text", errorText);
+    cy.get("#lastName + div + span").should("contain.text", errorText);
+    cy.get("#phone + div + span").should("contain.text", errorText);
+    cy.get("#email + div + span").should("contain.text", errorText);
 
-    cy.get("#select-3-input").should("have.class", "input-validation-error");
-    cy.get("#select-4-input").should("have.class", "input-validation-error");
+    // // These asserts will always fail as the id are generated dynamically
+    // // ... of course, unless you improve locators ... or use AI to generate them
+    // // Branch
+    // cy.get(
+    //   "#a78b0f0f-8519-43ce-8970-11366717caf1 ~ .css-rqtwkt + .css-184vw2j > span"
+    // ).should("contain.text", errorText);
+    // // Department
+    // cy.get(
+    //   "#b111f0a5-5c10-4c24-a5a6-c5dd29f4effa ~ .css-rqtwkt + .css-184vw2j span"
+    // ).should("contain.text", errorText);
   });
 
   it("Autonomous assert", () => {
+    cy.wopeeStartScenario("Autonomous assert");
     cy.visit(baseUrl);
     cy.get("button").contains("Odeslat registraci").click();
 
     // Validate mandatory fields
     cy.wopeeTrack("Check mandatory fields");
+    cy.wopeeStopScenario();
   });
 });
